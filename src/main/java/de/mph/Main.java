5585a6b67c8c040630905725a9527adfdd8fc3c3
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-28 08:18:28 -0400

    Updated Commit

--- src/main/java/de/mph/Main.java
+++ src/main/java/de/mph/Main.java
@@ -1,7 +1,24 @@
-package de.mph-web;
+package de.mph;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws  Exception{
+        final Document document= Jsoup.connect("https://www.nasdaq.com/market-activity/quotes/historical").get();
+       // System.out.println(document.outerHtml());
+      for(Element row:document.select(".manual-table")){
+
+
+   final  String Symbol=row.select("row_0 col_0").text();
+   final String CompanyName=row.select("row_0 col_1").text();
+
+  System.out.println(Symbol+"Symbol      "+CompanyName+"Company Name");
+
+        }
+
+
+
     }
 }
